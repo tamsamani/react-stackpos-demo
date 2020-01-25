d1642ba8f050e37fe0c7d28b2e3e6eb636ca6406
tamsamani <tamsmoha@gmail.com>
2020-01-25 03:33:42 +0100

    2 files modified, 1 added, 2 deleted

--- index.js
+++ index.js
@@ -1,26 +1,24 @@
-import React, { Component } from 'react';
-import { render } from 'react-dom';
-import Hello from './Hello';
-import './style.css';
+import React from "react";
+import { render } from "react-dom";
 
-class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      name: 'React'
-    };
-  }
+import "./index.scss";
 
-  render() {
-    return (
-      <div>
-        <Hello name={this.state.name} />
-        <p>
-          Start editing to see some magic happen :)
-        </p>
+const StackCard = ({ className, children, ...props }) => (
+  <div className={className + " p-1 rounded shadow"} {...props}>
+    {children}
+  </div>
+);
+
+function App() {
+  return (
+    <div className="relative bg-gray-100 h-screen w-screen">
+      <div className="text-lg font-bold text-center"> HEADER POKE GAME </div>
+      <div className="m-1 p-1 rounded bg-red-200">
+        <StackCard>Name 2</StackCard>
+        <StackCard>Name 3</StackCard>
       </div>
-    );
-  }
+    </div>
+  );
 }
 
-render(<App />, document.getElementById('root'));
+render(<App />, document.getElementById("root"));
