e7fddf4cc711694966cbddee6b05f22ab0f831fb
tamsamani <tamsmoha@gmail.com>
2020-01-25 12:24:47 +0100

    1 file modified

--- data/monster.mod.ts
+++ data/monster.mod.ts
@@ -15,12 +15,33 @@ interface MonsterFormModule {
 
 }
 
+interface MonsterEvolutionRequirement {
+
+}
+
+interface MonsterEvolutionSerie {
+  id: Number;
+  starter: MonsterModule;
+  list: Array<{
+    index: Number;
+    path: Number;
+    monster: MonsterModule;
+    requirement: MonsterEvolutionRequirement
+  }>;
+}
+
+interface MonsterEvolution {
+  serie: Array<MonsterEvolutionSerie>;
+  index: Number;
+}
+
 // the Basic Monster Module
 interface MonsterModule {
-  id: Number;
-  name: String;
-  spiece: MonsterSpeiceModule;
+  id: Number; // identical number for monster
+  name: String; // name of monster
+  spiece: MonsterSpeiceModule; // the specei 
   types: Array<MonsterTypeModule>;
   family: MonsterFamilyModule;
   forms: Array<MonsterFormModule>;
+  evolution: MonsterEvolution;
 }
