e7a2786b31db8e061fb7e912b82ab4f634aa5b58
tamsamani <tamsmoha@gmail.com>
2020-01-25 12:40:22 +0100

    Monster Modules #3

--- data/monster.mod.ts
+++ data/monster.mod.ts
@@ -20,19 +20,20 @@ interface MonsterEvolutionRequirement {
 }
 
 interface MonsterEvolutionSerie {
-  id: Number;
-  starter: MonsterModule;
+  id: Number; // identify evolution serie
+  starter: MonsterModule; // the starter monster
   list: Array<{
-    index: Number;
-    path: Number;
-    monster: MonsterModule;
-    requirement: MonsterEvolutionRequirement
+    index: Number; // index of evoultion
+    path: Number; // path line of evolution
+    monster: MonsterModule; // next evolved monster
+    requirement: MonsterEvolutionRequirement; // requirement for evolve this monster
   }>;
 }
 
 interface MonsterEvolution {
-  serie: Array<MonsterEvolutionSerie>;
-  index: Number;
+  serie: Array<MonsterEvolutionSerie>; //related serie of evolution
+  index: Number; // actual index of evolution
+  path: Number; // path of evolution for this monster
 }
 
 // the Basic Monster Module
